1a219decdd075861bff368b54f85fc15edb42ce0
RisheDubs <rishedubs@gmail.com>
2026-04-11 19:55:53 +1000

    feat: add main dispatcher to route run and child commands

--- cmd/byocontainer/main.go
+++ cmd/byocontainer/main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import(
+    "fmt"
+    "os"
+
+    "github.com/rishekesh/byoContainerGo/internal/container"
+)
+
+func main() {
+	if len(os.Args) < 2{
+		fmt.Fprintln(os.Stderr, "usage: byocontainer run <cmd>")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "run":
+		container.Parent(os.Args[2:])
+	case "child":
+		container.Child(os.Args[2:])
+	default:
+		fmt.Fprintf(os.Stderr, "unknown command: %s\n", os.Args[1])
+		os.Exit(1)
+	}
+}
