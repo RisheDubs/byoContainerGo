f25f71d0d1879443d04d23fc0ee781b18bf7a856
Rishekesh.B <69071638+RisheDubs@users.noreply.github.com>
2026-04-20 19:50:44 +1000

    Update cgroups.go

--- internal/cgroups/cgroups.go
+++ internal/cgroups/cgroups.go
@@ -1,17 +1,1 @@
 package cgroups
-
-import (
-	"os/exec"
-	"testing"
-)
-
-func TestRunRequiresArguments(t *testing.T) {
-	cmd := exec.Command("go", "run",
-		"./cmd/byocontainer/", "invalid")
-	out, _ := cmd.CombinedOutput()
-	if len(out) == 0 {
-		t.Error("expected error output for invalid command")
-	}
-
-
-}
